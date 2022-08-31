c781e07f74ee44b49bbd838c7bdb88e87bb184f2
Yy2z <1615978027@qq.com>
2022-08-31 10:05:36 +0800

    引入并全局element-plus、图标

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import *as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
+// 注册Icons 全局组件
+Object.keys(ElementPlusIconsVue).forEach(key => {
+    app.component(key, ElementPlusIconsVue[key])
+  })
+
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,8 @@
-
 <template>
-    <div>
-        主页
-    </div>
+  <div>
+    主页
+    <el-icon color="#409EFC" class="no-inherit">
+  <share />
+</el-icon>
+  </div>
 </template>
-
