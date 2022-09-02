fae97538fdf365101b0d8c840134051c3413b9be
Yy2z <1615978027@qq.com>
2022-09-02 17:12:25 +0800

    header

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,28 @@
 <template>
-    <div class="common-layout">
-      <el-container>
-        <el-header>
-            <Header />
-        </el-header>
-        <el-main>
-            <router-view />
-        </el-main>
-      </el-container>
-    </div>
-  </template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <Header />
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </div>
+</template>
 
-  <script setup>
-    import Header from '../components/header.vue'
-  </script>
+<script setup>
+import Header from "../components/header.vue";
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.common-layout,
+.el-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
