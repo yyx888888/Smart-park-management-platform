8d5c7de90d8f7f46536ddfd64a18cc44e42f0032
aimeewang <3463723755@qq.com>
2022-09-03 14:44:17 +0800

    添加qualification.vue

---
+++ src/views/home/Qualification.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <VChart class="chart" :option="option"/>
+  </div>
+</template>
+
+<script>
+import VChart from "vue-echarts";
+import { ref } from "vue";
+
+export default ({
+  components: {
+    VChart
+  },
+  setup: () => {
+    const option = ref({
+      legend: {
+        textStyle:{
+          color:'white'
+        }
+      },
+      tooltip: {},
+      dataset: {
+        source: [
+          ['product', '科技企业', '高新企业'],
+          ['2015', 800, 550],
+          ['2016', 300, 350],
+          ['2017', 400, 850],
+          ['2018', 750, 650],
+          ['2019', 500, 660],
+          ['2020', 660, 850],
+          ['2021', 500, 830],
+        ]
+      },
+      xAxis: {
+        type: 'category' ,
+        axisLabel : {
+          textStyle: {
+            color: '#fff'
+          }
+        }
+      },
+      yAxis: {
+        axisLabel : {
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        splitLine:{ //修改背景线条样式
+          show:true,//是否展示
+          lineStyle:{
+            color:"rgb(15,19,35,0.2)",//线条颜色
+          }
+        }
+      },
+      series: [
+        { type: 'bar' },
+        {
+          type: 'bar',
+          itemStyle:{
+            normal:{
+              color: '#ffa041'
+            }
+          }
+        },
+      ]
+    });
+    return { option };
+  }
+})
+</script>
+
+<style scoped>
+.chart{
+  height: 300px;
+}
+</style>
