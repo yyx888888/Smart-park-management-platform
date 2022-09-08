f98f7674dabed58e81db8b079a63a7aee55b904f
杨永鑫 <1292924111@qq.com>
2022-09-08 11:11:57 +0800

    updata

--- src/router/index.js
+++ src/router/index.js
@@ -1,67 +1,56 @@
-import { createRouter, createWebHashHistory } from 'vue-router' 
+import { createRouter, createWebHashHistory } from 'vue-router'
 const manageroutes = [
-<<<<<<< Updated upstream
-   {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../views/login.vue'),
-   },
-   {
-      path: '/',
-      name: '/',
-      component: () => import('../layout/index.vue'),
-      children:[
-=======
+
   {
     path: '/login',
     name: 'Login',
     component: () => import('../views/login.vue'),
   },
   {
-    path: '/home',
-    name: '/home',
+    path: '/',
+    name: '/',
     component: () => import('../layout/index.vue'),
     children: [
->>>>>>> Stashed changes
-      {
-            path: '/home',
-            name: 'home',
-            component: () => import('../views/home.vue'),
-      },
-      {
-         path: '/configuration',
-         name: 'configuration',
-         component: () => import('../views/configuration.vue'),
-      },
-      {
-         path: '/operation',
-         name: 'operation',
-         component: () => import('../views/operation.vue')
-      }, 
-      {
-         path: '/property',
-         name: 'property',
-         component: () => import('../views/property.vue')
-      },
-      {
-         path: '/visualization',
-         name: 'visualization',
-         component: () => import('../views/visualization.vue')
-      },
-      
- ]
- },
 
-] 
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('../views/home.vue'),
+      },
+      {
+        path: '/configuration',
+        name: 'configuration',
+        component: () => import('../views/configuration.vue'),
+      },
+      {
+        path: '/operation',
+        name: 'operation',
+        component: () => import('../views/operation.vue')
+      },
+      {
+        path: '/property',
+        name: 'property',
+        component: () => import('../views/property.vue')
+      },
+      {
+        path: '/visualization',
+        name: 'visualization',
+        component: () => import('../views/visualization.vue')
+      },
+
+    ]
+  },
+
+]
 
 // 哈希路由
 const router = createRouter({
-   // 路由模式
- history: createWebHashHistory(),
- routes: manageroutes,
-}) 
+  // 路由模式
+  history: createWebHashHistory(),
+  routes: manageroutes,
+})
 /**
 
 - 输出对象
    */
-  export default router;
+export default router;
