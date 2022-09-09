6b1ce2fb5a5b73677565ed469548bd0b0e28c098
aimeewang <3463723755@qq.com>
2022-09-09 21:09:40 +0800

    'css样式修改'

--- src/views/visualization/DeviceManagement.vue
+++ src/views/visualization/DeviceManagement.vue
@@ -1,10 +1,507 @@
+<!--园区设备管理-->
 <template>
-  <h1>园区设备管理</h1>
+  <div class="deviceBox">
+<!--    左侧可视区-->
+    <div class="column">
+<!--      设备资产数量-->
+      <div>
+        <div class="title">设备资产数量</div>
+        <div class="allCount">
+          <div class="deviceCount">1500</div>
+          <div class="subTitle">设备数量总数（件）</div>
+        </div>
+        <div class="myChart1" ref="myChart1"></div>
+      </div>
+<!--      供配电设备运行状态-->
+        <div class="title">供配电设备运行状态</div>
+        <div class="myChart2" ref="myChart2"></div>
+<!--      夜间照明-->
+        <div class="title lightBox">夜间照明</div>
+        <div class="turnLight">
+          <div class="turnLightItem">
+            <img src="../../../public/images/light.png" alt="">
+            <div class="turnLightItemTitle">景灯1</div>
+            <el-switch
+                @change="test" size="large" on-value="1" off-value="0" v-model="value1" style="--el-switch-on-color: #ffe400">
+            </el-switch>
+          </div>
+          <div class="turnLightItem">
+            <img src="../../../public/images/light.png" alt="">
+            <div class="turnLightItemTitle">景灯2</div>
+            <el-switch
+                @change="test" size="large" on-value="1" off-value="0" v-model="value2" style="--el-switch-on-color: #ffe400">
+            </el-switch>
+          </div>
+          <div class="turnLightItem">
+            <img src="../../../public/images/light.png" alt="">
+            <div class="turnLightItemTitle">景灯3</div>
+            <el-switch
+                @change="test" size="large" on-value="1" off-value="0" v-model="value3" style="--el-switch-on-color: #ffe400">
+            </el-switch>
+          </div>
+          <div class="turnLightItem">
+            <img src="../../../public/images/light.png" alt="">
+            <div class="turnLightItemTitle">景灯4</div>
+            <el-switch
+                @change="test" size="large" on-value="1" off-value="0" v-model="value4" style="--el-switch-on-color: #ffe400">
+            </el-switch>
+          </div>
+        </div>
+    </div>
+<!--    右侧可视区-->
+    <div class="column columnTwo">
+      <div class="titleBox">
+<!--        供水系统-->
+        <div class="title">供水系统</div>
+        <div class="waterSupply">
+          <div class="waterSupplyItem">
+            <div class="itemTitle">生活水池</div>
+            <div class="itemNo">3.1</div>
+          </div>
+          <div class="waterSupplyItem">
+            <div class="itemTitle">消防水池</div>
+            <div class="itemNo">2.6</div>
+          </div>
+          <div class="waterSupplyItem">
+            <div class="itemTitle">高位水箱</div>
+            <div class="itemNo">4.5</div>
+          </div>
+
+        </div>
+<!--        供水系统滚动显示屏-->
+        <div class="waterRoll">
+          <div class="waterRollTitle">
+            <div>设备名称</div>
+            <div>储水量</div>
+            <div>发生时间</div>
+          </div>
+          <div @mousemove="mmove" @mouseleave="mend" >
+            <div id="infiniteScroll" style="height: 150px;overflow: hidden">
+              <div class="infiniteScrollItem">
+                <div>东南区域</div>
+                <div>12300</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>西南区域</div>
+                <div>12000</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>西北区域</div>
+                <div>11100</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>东北区域</div>
+                <div>12100</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>华北区域</div>
+                <div>10000</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>华南区域</div>
+                <div>11000</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>苏皖区域</div>
+                <div>12000</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>东北区域</div>
+                <div>12000</div>
+                <div>09/22</div>
+              </div>
+              <div class="infiniteScrollItem">
+                <div>西南区域</div>
+                <div>10200</div>
+                <div>09/22</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+<!--        监控系统-->
+        <div class="title">监控系统</div>
+        <div class="monitor"></div>
+<!--        供暖系统-->
+        <div class="title">供暖系统</div>
+        <div class="circleBox">
+          <div class="circleImg">
+            <img src="../../../public/images/circle.svg" alt="" class="circle">
+            <img src="../../../public/images/circle5.svg" alt="" class="circle5">
+            <img src="../../../public/images/circle4.svg" alt="" class="circle4">
+            <img src="../../../public/images/circle3.svg" alt="" class="circle3">
+            <img src="../../../public/images/circle2.svg" alt="" class="circle2">
+            <img src="../../../public/images/circle1.svg" alt="" class="circle1">
+          </div>
+          <div class="circlePer">45%</div>
+          <div class="circleTitle">供暖压力</div>
+        </div>
+        <el-table
+            :data="tableData"
+            style="width: 48%"
+            class="tableData"
+            :header-cell-style="{backgroundColor:'rgba(0,0,0,0.9)',color:'#ffe400',border:'none'}"
+            :row-style="{backgroundColor: 'rgba(255,255,255,0)'}"
+            :cell-style="{borderColor: 'rgba(255,228,0,0)'}"
+        >
+          <el-table-column prop="name" label="设备名称" width="115" align="center"/>
+          <el-table-column prop="status" label="设备状态" width="90" align="center"/>
+        </el-table>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+
+const tableData = [
+  {
+    name:'A1楼地暖设备',
+    status:'正常'
+  },
+  {
+    name:'A2楼地暖设备',
+    status:'正常'
+  },
+  {
+    name:'A3楼地暖设备',
+    status:'正常'
+  }
+]
+
+export default {
+  data(){
+    return{
+      value1:'1',
+      value2:'1',
+      value3:'1',
+      value4:'1',
+      tableData,
+      shishi: null
+    }
+  },
+  mounted() {
+    let myChart1=echarts.init(this.$refs.myChart1);
+    let myChart2=echarts.init(this.$refs.myChart2);
+    let option1 = {
+      title: {
+        text:'1,500',//主标题文本
+        subtext:'正常运行设备',//副标题文本
+        left:'32%',
+        top:'36%',
+        textStyle:{
+          fontSize: 16,
+          color:'#ffe400',
+          align:'center'
+        },
+        subtextStyle:{
+          fontFamily : "微软雅黑",
+          fontSize: 10,
+          color:'#fafafa',
+          align:'center'
+        }
+      },
+      legend: {
+        orient: 'vertical',
+        top: 'center',
+        right: 30,
+        itemGap: 30,
+        textStyle: {
+          color: '#fff'
+        }
+      },
+      series: [
+        {
+          type: 'pie',
+          radius: ['40%', '70%'],
+          center: ['40%','50%'],
+          avoidLabelOverlap: true,
+
+          itemStyle: {
+            borderColor: '#fff',
+            borderWidth: 1
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: '20',
+              fontWeight: 'bold'
+            }
+          },
+          data: [
+            { value: 1048, name: '供配电' ,itemStyle:{color: '#073574'}},
+            { value: 735, name: '给排水' ,itemStyle:{color: '#166872'}},
+            { value: 580, name: '暖通' ,itemStyle:{color: '#227a3d'}},
+            { value: 484, name: '供热' ,itemStyle:{color: '#953345'}},
+          ]
+        }
+      ]
+    };
+    let option2 = {
+      xAxis: {
+        type: 'category',
+        data: ['11/20', '11/22', '11/24', '11/26', '11/28'],
+        axisLabel: {
+          color: '#0159d5'
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            type: 'solid',
+            color: '#807f7f'
+          }
+        }
+      },
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+          color: '#0159d5'
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            type: 'solid',
+            color: '#807f7f'
+          }
+        }
+      },
+      series: [
+        {
+          data: [100, 650, 810, 108, 300],
+          type: 'line',
+          smooth: true
+        }
+      ]
+    };
+    myChart1.setOption(option1);
+    myChart2.setOption(option2);
+  },
+
+  beforeDestroy() {
+    //清除定时器
+    clearInterval(this.shishi)
+  },
+  destroyed() {
+    //清除定时器
+    clearInterval(this.shishi)
+  },
+  created() {
+    //启动定时器
+    this.start()
+  },
+  methods: {
+    test (val) {
+      // console.log(val)
+    },
+    //鼠标移动事件
+    mmove() {
+      clearTimeout(this.shishi)
+    },
+    //鼠标移出事件
+    mend() {
+      this.start()
+    },
+    start() {
+      //清除定时器
+      clearTimeout(this.shishi)
+      //定时器触发周期
+      let speed = 75
+      this.shishi = setInterval(this.MarqueeTest, speed)
+    },
+    MarqueeTest() {
+      let that =this;
+      let test1 = document.getElementById('infiniteScroll')
+      //判断组件是否渲染完成
+      if (test1.offsetHeight == 0) {
+        test1 = document.getElementById('infiniteScroll')
+      } else {
+        //如果列表数量过少不进行滚动
+        if(test1.childNodes.length<6){
+          clearTimeout(that.shishi)
+          return;
+        }
+        //组件进行滚动
+        test1.scrollTop += 1
+        //判断滚动条是否滚动到底部
+        if (test1.scrollTop == (test1.scrollHeight - test1.clientHeight)) {
+          //获取组件第一个节点
+          let a = test1.childNodes[0]
+          //删除节点
+          test1.removeChild(a)
+          //将该节点拼接到组件最后
+          test1.append(a)
+        }
+      }
+    }
+  }
+}
+
 </script>
 
 <style scoped>
-
+.deviceBox{
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  background: url("../../../public/images/deviceManagementBanner.png");
+  background-size: cover;
+}
+.column{
+  width: 25%;
+  height: 100%;
+  background: url("../../../public/images/deviceBackgroundimage1.svg");
+}
+.columnTwo{
+  transform: rotateY(180deg);
+}
+.titleBox{
+  transform: rotateY(180deg);
+}
+.title{
+  color: #fff;
+  font-size: 20px;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  margin-top: 2vh;
+  border-bottom: 1px solid wheat;
+  padding-bottom: 1vh;
+}
+.allCount{
+  width: 100%;
+  height: 5vh;
+  text-align: center;
+}
+.deviceCount{
+  margin: 5px auto;
+  width: 25%;
+  font-size: 26px;
+  font-weight: 1000;
+  color: #ffe400;
+  border-bottom: 1px solid gray;
+}
+.subTitle{
+  font-size: 14px;
+  color: #d9d7d7;
+}
+.myChart1{
+  height: 20vh;
+}
+.myChart2{
+  height: 30vh;
+}
+.lightBox{
+  margin-top: -4vh;
+}
+.turnLight{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 2vh;
+}
+.turnLightItem{
+  display: flex;
+  /*flex-wrap: wrap;*/
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.turnLightItemTitle{
+  font-size: 20px;
+  color: #ffe400;
+}
+.waterSupply{
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  text-align: center;
+}
+.itemTitle{
+  width: 6rem;
+  height: 4vh;
+  line-height: 4vh;
+  background-image: linear-gradient(to right,#0b0f23,#0099ff,#0b0f23);
+  color: #fff;
+  border-right:2px solid  #0099ff;
+  border-left:2px solid  #0099ff;
+  margin-top: 1vh;
+}
+.itemNo{
+  font-size: 26px;
+  font-weight: 1000;
+  color: #ffe400;
+}
+.waterRollTitle{
+  display: flex;
+  justify-content: space-around;
+  color: #0099ff;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  padding-bottom: 1vh;
+  border-bottom: 1px solid #0099ff;
+}
+.monitor{
+  height: 15vh;
+  background: url("../../../public/images/monitor.png") no-repeat;
+  margin-left: 1.6rem;
+  margin-top: 1vh;
+}
+.circle{
+  position: absolute;
+  bottom: 0;
+}
+.circle1,.circle2,.circle3,.circle4,.circle5{
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
+}
+.circleBox{
+  width: 180px;
+  height: 180px;
+  position: relative;
+  margin-top: 2vh;
+  margin-left: 0.5rem;
+}
+.circlePer{
+  color: #fff;
+  font-size: 26px;
+  font-weight: 800;
+  position: absolute;
+  left: 3.8rem;
+  top: 2.2vh;
+}
+.circleTitle{
+  color: #fff;
+  font-size: 18px;
+  position: absolute;
+  bottom: 4vh;
+  left: 3.3rem;
+}
+.tableData{
+  background-image: linear-gradient(to right,#0b0f23,#0099ff,#0b0f23);
+  color: #fff;
+  border-right:2px solid  #0099ff;
+  border-left:2px solid  #0099ff;
+  position: absolute;
+  bottom: 1vh;
+  left: 12.5rem;
+}
+.infiniteScrollItem{
+  display: flex;
+  color: #fff;
+  justify-content: space-around;
+  margin-left: 1.4rem;
+  margin-right: 2rem;
+  padding-bottom: 1vh;
+}
 </style>
--- src/views/visualization/EnergyConsumption.vue
+++ src/views/visualization/EnergyConsumption.vue
@@ -1,10 +1,541 @@
+<!--能源消耗分析-->
 <template>
-  <h1>能源消耗分析</h1>
+  <div class="energyBox">
+<!--    左侧可视框-->
+    <div class="column" >
+<!--      绿色能源-->
+      <div class="columnItem">
+        <div class="title">绿色能源</div>
+        <div class="greenEnergy">
+          <div class="energyItem">
+            <img src="../../../public/images/energylogo.svg" alt="">
+            <div class="energyItemCont">
+              <div>充电桩数量</div>
+              <div>102个</div>
+            </div>
+          </div>
+          <div class="energyItem energyItemTwo">
+            <img src="../../../public/images/energylogo.svg" alt="">
+            <div class="energyItemCont">
+              <div>光伏发电量</div>
+              <div>102kWh</div>
+            </div>
+          </div>
+          <div class="energyItem">
+            <img src="../../../public/images/energylogo.svg" alt="">
+            <div class="energyItemCont">
+              <div>储能装机量</div>
+              <div>102MW</div>
+            </div>
+          </div>
+        </div>
+        <div class="myChart1" ref="myChart1"></div>
+      </div>
+<!--      能源类型-->
+      <div class="columnItem">
+        <el-table
+            :data="energyData"
+            style="width: 90%"
+            height="200"
+            class="tableData"
+            :header-cell-style="{border:'none',backgroundColor:'#162a41',color:'#b9eef8'}"
+            :row-style="{color:'#b9eef8',backgroundColor:'rgba(255,255,255,0)'}"
+            :cell-style="{borderColor: 'rgba(255,228,0,0)'}"
+        >
+          <el-table-column prop="type" label="类型" width="100" align="center" border="none"/>
+          <el-table-column prop="number" label="数量" width="160" align="center"/>
+          <el-table-column prop="proportion" label="占比" width="200" align="center" >
+            <el-progress :stroke-width="10" :percentage="energyData[3].proportion" color="#9a03d5"/>
+          </el-table-column>
+        </el-table>
+
+      </div>
+<!--      年度碳排放量趋势-->
+      <div class="columnItem">
+        <div class="title">年度碳排放量趋势</div>
+        <div class="myChart3" ref="myChart3"></div>
+      </div>
+    </div>
+<!--    中间可视框-->
+    <div class="column">
+<!--      banner-->
+      <div class="columnItemTwo"><div class="energyBanner"></div></div>
+<!--      园区能耗-->
+      <div class="columnItemTwo parkEnergy">
+        <div class="title">园区能耗</div>
+        <div class="myChart4" ref="myChart4"></div>
+      </div>
+    </div>
+<!--    右侧可视框-->
+    <div class="column">
+<!--      实时监测-->
+      <div class="columnItemThree">
+        <div class="title">实时监测</div>
+        <div class="myChart5" ref="myChart5"></div>
+      </div>
+<!--      企业低碳排名-->
+      <div class="columnItemThree companyNo">
+        <div class="title">企业低碳排名</div>
+        <el-table
+            :data="tableData"
+            style="width: 90%"
+            class="tableData"
+            :header-cell-style="{border:'none',backgroundColor:'#263750',color:'#fff'}"
+            :row-style="{backgroundColor: 'rgba(255,255,255,0)'}"
+            :cell-style="{borderColor: 'rgb(160,158,158)'}"
+        >
+          <el-table-column prop="id" label="排名" width="60" align="center"/>
+          <el-table-column prop="name" label="企业名称" width="160" align="center"/>
+          <el-table-column prop="address" label="企业地址" width="100" align="center"/>
+          <el-table-column prop="total" label="单位产值碳排放" width="150" align="center"/>
+        </el-table>
+
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import * as echarts from "echarts";
+
+const tableData = [
+  {
+    id: '1',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '2',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '3',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '4',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '5',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '6',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '7',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '8',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '9',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+  {
+    id: '10',
+    name: '浙江XX科技有限公司',
+    address: 'A1幢502室',
+    total: '115.30'
+  },
+];
+const energyData=[
+  {
+    type:'工厂用电',
+    number:'1000',
+    proportion:98
+  },
+  {
+    type:'工厂用煤',
+    number:'950',
+    proportion:88
+  },
+  {
+    type:'员工充电',
+    number:'850',
+    proportion:65
+  },
+  {
+    type:'日常照明',
+    number:'750',
+    proportion:55
+  },
+  {
+    type:'设备未关',
+    number:'650',
+    proportion:48
+  },
+  {
+    type:'光伏发电',
+    number:'600',
+    proportion:40
+  }
+
+];
+export default {
+  data(){
+    return{
+      tableData,
+      energyData,
+    }
+  },
+  mounted() {
+    let myChart1=echarts.init(this.$refs.myChart1);
+    let myChart3=echarts.init(this.$refs.myChart3);
+    let myChart4=echarts.init(this.$refs.myChart4);
+    let myChart5=echarts.init(this.$refs.myChart5);
+    let option1={
+      tooltip: {
+        trigger: 'axis'
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '1%',
+        top: '4%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        axisLabel: {
+          color: '#fff'
+        },
+        boundaryGap: true,
+        data: ['12/15', '12/16', '12/17', '12/18', '12/19', '12/20']
+      },
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+          color: '#fff'
+        },
+        splitLine: {
+          lineStyle: {
+            // 设置背景横线
+            color: '#454545FF'
+          }
+        },
+      },
+      series: [
+        {
+          name: '光伏发电量',
+          type: 'line',
+          smooth: true,
+          stack: 'Total',
+          data: [500, 410, 920, 290, 750, 810]
+        },
+        {
+          name: '减排量',
+          type: 'line',
+          smooth: true,
+          stack: 'Total',
+          data: [760, 610, 1249, 900, 910, 1000]
+        }
+      ]
+
+    };
+    let option3={
+
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '1%',
+        top: '15%',
+        containLabel: true
+      },
+      legend: {
+        top: 'center',
+        right: 30,
+        itemGap: 30,
+        textStyle: {
+          color: '#fff'
+        }
+      },
+      xAxis: {
+        type: 'category',
+        axisLabel: {
+          color: '#fff'
+        },
+        boundaryGap: true,
+        data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+      },
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+          color: '#fff'
+        },
+        splitLine: {
+          lineStyle: {
+            // 设置背景横线
+            color: '#454545FF'
+          }
+        },
+      },
+      series: [
+        {
+          // name:'2021年',
+          data: [200,250,350,450,350,480,410,510,400,450,350,410],
+          type: 'line',
+          smooth: true,
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(
+                0, 1, 0, 0,
+              [
+                {offset: 1, color: '#765ec1'},
+                {offset: 0, color: '#04d3d6'}
+              ]
+            )
+          },
+          lineStyle: {
+            color:'#8868e9'
+          }
+        }
+      ]
+    };
+    let option4={
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '1%',
+        top: '15%',
+        containLabel: true
+      },
+      legend: {
+        textStyle:{
+          color:'#fff'
+        }
+      },
+      tooltip: {},
+      dataset: {
+        source: [
+          ['product', '煤炭消耗', '电力消耗'],
+          ['08/01', 800, 550],
+          ['08/02', 300, 350],
+          ['08/03', 400, 850],
+          ['08/04', 750, 650],
+          ['08/05', 500, 660],
+          ['08/06', 660, 850],
+          ['08/07', 500, 830],
+          ['08/08', 500, 830],
+        ]
+      },
+      xAxis: {
+        type: 'category' ,
+        axisLabel : {
+          color: '#fff'
+        }
+      },
+      yAxis: {
+        axisLabel : {
+          color: '#fff'
+        },
+        splitLine:{ //修改背景线条样式
+          show:false,//是否展示
+        }
+      },
+      series: [
+        { type: 'bar',
+          itemStyle:{
+            color: '#0ac0d7'
+          }
+        },
+        {
+          type: 'bar',
+          itemStyle:{
+            color: new echarts.graphic.LinearGradient(
+                0, 1, 0, 0,
+                [
+                  {offset: 1, color: '#2277d9'},
+                  {offset: 0, color: '#513feb'}
+                ]
+            )
+          }
+        },
+      ]
+    };
+    let option5={
+      legend: {
+        data: ['电力负荷', '实时电压'],
+        textStyle:{
+          color:'#fff'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          axisLabel : {
+            color: '#fff'
+          },
+          boundaryGap: true,
+          data: ['18:00', '18:05', '18:10', '18:15', '18:20' ,'18:25']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          axisLabel : {
+            color: '#fff'
+          },
+          splitLine: {
+            lineStyle: {
+              // 设置背景横线
+              color: '#454545FF'
+            }
+          },
+        }
+      ],
+      series: [
+        {
+          name: '电力负荷',
+          type: 'line',
+          smooth: true,
+          stack: 'Total',
+          areaStyle: {
+            color:'rgba(60,144,124,0.5)'
+          },
+          emphasis: {
+            focus: 'series'
+          },
+          data: [1700, 1150, 400, 300, 950, 940]
+        },
+        {
+          name: '实时电压',
+          type: 'line',
+          smooth:true,
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'top'
+          },
+          areaStyle: {
+            color:'rgba(61,100,173,0.5)'
+          },
+          emphasis: {
+            focus: 'series'
+          },
+          data: [820, 510, 100, 109, 450, 440]
+        }
+      ]
+    };
+    myChart1.setOption(option1);
+    myChart3.setOption(option3);
+    myChart4.setOption(option4);
+    myChart5.setOption(option5);
+  }
+}
+
 </script>
 
 <style scoped>
-
+  .energyBox{
+    width: 100%;
+    height: 100%;
+    background-color: #19274c;
+    display: flex;
+    min-width: 1024px;
+    max-width: 1920px;
+  }
+  .column{
+    flex: 3.25;
+    display: flex;
+    flex-direction: column;
+  }
+  :nth-child(2){
+    flex: 3.5;
+  }
+  .columnItem{
+    flex: 3.33;
+    background-color: #162a41;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 10px;
+    margin: 10px;
+  }
+  .columnItemTwo{
+    flex: 6.8;
+    background-color: #162a41;
+    margin: 10px;
+  }
+  .energyBanner{
+    width: 100%;
+    height: 100%;
+    background-image: url("../../../public/images/energyBanner.png");
+  }
+  .parkEnergy{
+    flex: 3.2;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 10px;
+  }
+  .columnItemThree{
+    flex: 3.3;
+    background-color: #162a41;
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 10px;
+    margin: 10px;
+  }
+  .companyNo{
+    flex: 6.7;
+  }
+  .title{
+    color: #fff;
+    font-size: 20px;
+    margin-top: 1.5vh;
+    margin-left: 2rem;
+  }
+  .greenEnergy{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin: 2vh;
+  }
+  .energyItem{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  .energyItemTwo{
+    margin-left: 4rem;
+  }
+  .energyItemCont{
+    margin-left: 1rem;
+    color: #fff;
+    font-size: 14px;
+  }
+  .myChart1{
+    height: 14.5vh;
+  }
+  .myChart3,.myChart4,.myChart5{
+    height: 22vh;
+  }
+  .tableData{
+    margin: 1vh auto;
+    background-color: rgba(255, 192, 203, 0);
+    color: #fff;
+  }
 </style>
